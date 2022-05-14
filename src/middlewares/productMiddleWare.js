a2d4f75f3048cd5c15b7bbf37e2c183802305860
Gleison Moura <gle.bar.mou@gmail.com>
2022-05-14 15:41:16 -0300

    alterando dados para testar heroku

--- src/middlewares/productMiddleWare.js
+++ src/middlewares/productMiddleWare.js
@@ -1,13 +1,14 @@
 import joi from "joi";
+import chalk from "chalk";
 
 export async function validateProduct(req, res, next) {
     try {
         const product = req.body;
 
         const schemaProduct = joi.object({
+            name: joi.string().required(),
             image: joi.string().required(),
             price: joi.string().required(),
-            size: joi.string().required(),
             brand: joi.string().required(),
             category: joi.string().required(),
             date: joi.string().required()
@@ -16,8 +17,10 @@ export async function validateProduct(req, res, next) {
         const validation = schemaProduct.validate(product);
 
         if (validation.error) {
+            console.log(chalk.red(validation.error.details));
             return res.sendStatus(422);
         }
+
     } catch (e) {
         console.log(chalk.red("Algo deu errado em midleware/validateProduct: " + e));
         return res.sendStatus(500);
